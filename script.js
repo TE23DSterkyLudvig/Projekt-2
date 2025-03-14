eb868bb9c40ae5ddf137f388aa52663bb840a90c
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-03-14 14:38:08 +0100

    Jag har fortsatt med Javascript

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-
-
+let priser = [];
 let produkter = [];
-let summa = [];
 
-let pris = document.querySelector(".pris").textContent;
-let produkt = document.querySelector(".produkt_rubrik").textContent;
+let summa = 0;
 
 
 
-function lägga_till3060() {
+function lägga_till(produkt, pris){
+if(produkt == "GPU","CPU")
 
-}
-
-function lägga_till7900() {
-
-}
-
-function lägga_tilli5() {
-
-}
-
-function lägga_tillcorsair() {
-
-}
-
-function lägga_tillartic() {
-
-}
-
-function lägga_tillmortar() {
 
 }
