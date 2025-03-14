3a7c7f37878337103f341e3cd9adc7a6fa215a2c
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-03-14 09:42:24 +0100

    Börjat med javascript

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 
 
 let produkter = [];
-let 
+let summa = [];
+
+let pris = document.querySelector(".pris").textContent;
+let produkt = document.querySelector(".produkt_rubrik").textContent;
+
+
+
+function lägga_till3060() {
+
+}
+
+function lägga_till7900() {
+
+}
+
+function lägga_tilli5() {
+
+}
+
+function lägga_tillcorsair() {
+
+}
+
+function lägga_tillartic() {
+
+}
+
+function lägga_tillmortar() {
+
+}
