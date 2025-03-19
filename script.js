82999ae6e5c9f768ed9a7df70af018c8fb3f505f
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-03-19 22:20:51 +0100

    Jag har blivit klar med att lägga till varorna i varukorgen och lagt till varorna i local storage. dock vet jag int ehur jag ska spara varukorgen.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 let priser = [];
 let produkter = [];
 
-let summa = 0;
+let summering = 0;
 
 
 
 function lägga_till(produkt, pris){
-if(produkt == "GPU","CPU")
+
+
+if(produkt == "3600","7900","i5","RMX","Artic","Mortar")
+    {
+        produkter.push(produkt);
+        console.log("Du la till 1", "" + produkt , "i varukorgen")
+       localStorage.setItem("varor", JSON.stringify(produkter));
+}
+if(pris == 3000,8000,7000,800,700,3500)
+    {
+    priser.push(pris);
+    console.log(produkt + "" + "kostade" + "" + pris +  " "  + "kr")
+    localStorage.setItem("priser", JSON.stringify(priser));
+    summering += pris;
+    console.log("Summa:" + "" + summering + "kr");
+
+    let summa = document.querySelector("#summa");
+    summa.textContent = "Summa:" + summering + "kr";
+
+    let varukorg_lista = document.querySelector("#varulista");
+    let list_varor = document.createElement("li");
+    list_varor.textContent = produkt + ":" + pris + "kr";
+    varukorg_lista.appendChild(list_varor);
+}
+
 
 
 }
